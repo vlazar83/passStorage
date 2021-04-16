8d53b595bfcfd36fd408faff4f7912ef40f39a18
admin <lazar.viktor@gmail.com>
2021-04-16 20:30:16 +0200

    using react navigation

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={require("./assets/vault.png")} />
-      <Text>Open up App.js to start working on your app..!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+            <Image source={require("./assets/vault.png")} />
+            <Text>Open up App.js to start working on your app..!</Text>
+            <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+
+
   );
 }
 
