38904ba42aa4b730644498be2a34ff8048738b17
admin <lazar.viktor@gmail.com>
2021-04-16 20:19:08 +0200

    added main screen picture

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./assets/vault.png")} />
+      <Text>Open up App.js to start working on your app..!</Text>
       <StatusBar style="auto" />
     </View>
   );
