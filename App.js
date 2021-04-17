5f9a94da7ae04a858589eb252c8bbeec8e2417bb
admin <lazar.viktor@gmail.com>
2021-04-17 08:34:13 +0200

    listView on Screen

--- App.js
+++ App.js
@@ -4,115 +4,24 @@ import React from "react";
 import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-function StackScreen() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen2}
-        options={({ navigation, route }) => ({
-          headerTitle: (props) => <LogoTitle {...props} />,
-        })}
-      />
-    </Stack.Navigator>
-  );
-}
-
-function HomeScreen2({ navigation }) {
-  const [count, setCount] = React.useState(0);
-
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button onPress={() => setCount((c) => c + 1)} title="Update count" />
-      ),
-    });
-  }, [navigation]);
-
-  return <Text>Count: {count}</Text>;
-}
+import FlatListBasics from "./data/data.js";
 
 function HomeScreen({ navigation, route }) {
-  React.useEffect(() => {
-    if (route.params?.post) {
-      // Post updated, do something with `route.params.post`
-      // For example, send the post to the server
-    }
-  }, [route.params?.post]);
   return (
     <View style={styles.container}>
       <Image source={require("./assets/vault.png")} />
-      <Text>Open up App.js to start working on your app..!</Text>
+      <Text>Storage for your passwords</Text>
       <Button
-        title="Go to Details"
-        onPress={() =>
-          navigation.navigate("Details", {
-            itemId: 86,
-            otherParam: "anything you want here",
-          })
-        }
+        title="Let's get started"
+        onPress={() => navigation.navigate("Details")}
       />
-      <Button
-        title="Create post"
-        onPress={() => navigation.navigate("CreatePost")}
-      />
-      <Button
-        title="Update the title"
-        onPress={() => navigation.setOptions({ title: "Updated!" })}
-      />
-      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-function CreatePostScreen({ navigation, route }) {
-  const [postText, setPostText] = React.useState("");
-
-  return (
-    <>
-      <TextInput
-        multiline
-        placeholder="What's on your mind?"
-        style={{ height: 200, padding: 10, backgroundColor: "white" }}
-        value={postText}
-        onChangeText={setPostText}
-      />
-      <Button
-        title="Done"
-        onPress={() => {
-          // Pass and merge params back to home screen
-          navigation.navigate({
-            name: "Home",
-            params: { post: postText },
-            merge: true,
-          });
-        }}
-      />
-    </>
-  );
-}
-
 function DetailsScreen({ route, navigation }) {
-  const { itemId, otherParam } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push("Details", {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
+  return FlatListBasics();
 }
 
 const Stack = createStackNavigator();
@@ -133,27 +42,15 @@ export default function App() {
       >
         <Stack.Screen
           name="Home"
-          component={HomeScreen2}
+          component={HomeScreen}
           options={{
-            title: "My home",
-            headerRight: () => (
-              <Button
-                onPress={() => alert("This is a button!")}
-                title="Info"
-                color="#fff"
-              />
-            ),
+            title: "Welcome",
           }}
         />
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
           options={{ title: "Details" }}
-        />
-        <Stack.Screen
-          name="CreatePost"
-          component={CreatePostScreen}
-          options={{ title: "CreatePost" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ data/data.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          { key: "Devin" },
+          { key: "Dan" },
+          { key: "Dominic" },
+          { key: "Jackson" },
+          { key: "James" },
+          { key: "Joel" },
+          { key: "John" },
+          { key: "Jillian" },
+          { key: "Jimmy" },
+          { key: "Julie" },
+          { key: "Devin2" },
+          { key: "Dan2" },
+          { key: "Dominic2" },
+          { key: "Jackson2" },
+          { key: "James2" },
+          { key: "Joel2" },
+          { key: "John2" },
+          { key: "Jillian2" },
+          { key: "Jimmy2" },
+          { key: "Julie2" },
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+};
+
+module.exports = FlatListBasics;
