66d70395f9fb946df7a29cdc67d22292a33555e2
admin <lazar.viktor@gmail.com>
2021-04-17 20:44:43 +0200

    using expo SecureStore step 1

--- screens/AddNewPasswordScreen.js
+++ screens/AddNewPasswordScreen.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import * as SecureStore from "expo-secure-store";
+
+async function save(key, value) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+async function getValueFor(key) {
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    alert("🔐 Here's your value 🔐 \n" + result);
+  } else {
+    alert("No values stored under that key.");
+  }
+}
 
 function AddNewPasswordScreen({ navigation, route }) {
+  const [key, onChangeKey] = React.useState("PasswordKey");
+  const [value, onChangeValue] = React.useState("sampleData");
+
   return (
     <View style={styles.container}>
-      <Text>Add new password</Text>
+      <Text style={styles.paragraph}>Save an item, and grab it later!</Text>
+
+      <Button
+        title="Save this key/value pair"
+        onPress={() => {
+          save(key, value);
+          onChangeKey("Your key here");
+          onChangeValue("Your value here");
+        }}
+      />
+
+      <Text style={styles.paragraph}>🔐 Enter your key 🔐</Text>
+      <TextInput
+        style={styles.textInput}
+        onSubmitEditing={(event) => {
+          getValueFor(event.nativeEvent.text);
+        }}
+        placeholder="Enter the key for the value you want to get"
+      />
     </View>
   );
 }
