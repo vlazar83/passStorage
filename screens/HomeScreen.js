6cb4f235cdd1b94b23ceaaf503fda29ac350ded3
admin <lazar.viktor@gmail.com>
2021-04-22 18:00:40 +0200

    load records on Home Screen into StateHolder

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,48 @@ import React from "react";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import stateHolder from "..//StateHolder.js";
+import KEY_FOR_ARRAY_OF_UUIDS from "../utils/constants.js";
+import * as SecureStore from "expo-secure-store";
 
 class HomeScreen extends React.Component {
+  async loadRecordsIntoPasswordRecordsArray() {
+    promises = [];
+    console.log("Array before foreach:" + stateHolder.state.uuidArray);
+    stateHolder.state.uuidArray.forEach((element) => {
+      console.log(element);
+      promises.push(SecureStore.getItemAsync(element));
+    });
+
+    Promise.all(promises).then((responses) => {
+      responses.forEach((element) => {
+        console.log(element);
+        stateHolder.state.passwordRecordsArray.push({
+          id: JSON.parse(element).id + Math.random().toString(36).substring(7),
+          displayName: JSON.parse(element).displayName,
+          userID: JSON.parse(element).userID,
+          password: JSON.parse(element).password,
+        });
+      });
+    });
+  }
+
+  async loadRecords() {
+    try {
+      let arrayInStringFormat = await SecureStore.getItemAsync(
+        KEY_FOR_ARRAY_OF_UUIDS
+      );
+      stateHolder.state.uuidArray = JSON.parse(arrayInStringFormat);
+      console.log("responseUUIDs1: ", stateHolder.state.uuidArray);
+    } catch (error) {
+      console.log(error);
+    }
+    this.loadRecordsIntoPasswordRecordsArray();
+  }
+
+  componentDidMount() {
+    this.loadRecords();
+  }
+
   render() {
     return (
       <View style={styles.container}>
